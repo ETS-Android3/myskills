b968d34e639e52e884ba8d9aa05984f835401c20
Saymon Damásio <saymon.damasio95@gmail.com>
2022-03-31 19:08:27 -0300

    feat: add greeting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet, TextInput, FlatList} from 'react-native';
-import {useState} from 'react/cjs/react.development';
 import {Button} from '../components/Button';
 import {SkillCard} from '../components/SkillCard';
 
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [skills, setSkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good morning');
+    } else if (currentHour <= 12) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good evening');
+    }
+  }, []);
 
   function handleAddNewSkill() {
     setSkills(oldSkills => [...oldSkills, newSkill]);
@@ -17,6 +29,7 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Saymon</Text>
+      <Text style={styles.greeting}>{greeting}</Text>
 
       <TextInput
         style={styles.input}
@@ -58,5 +71,9 @@ const styles = StyleSheet.create({
     padding: 15,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greeting: {
+    fontSize: 16,
+    color: '#fff',
   },
 });
