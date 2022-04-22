23e927e1daa4c12038341192b44ea83d253eacee
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-22 15:46:47 -0300

    feawt: dist to ios

--- ios/myskills/AppDelegate.mm
+++ ios/myskills/AppDelegate.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -26,6 +27,7 @@
 #endif
 
 @implementation AppDelegate
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -54,6 +56,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
+  
   return YES;
 }
 
